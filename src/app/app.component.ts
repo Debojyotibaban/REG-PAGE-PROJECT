aab159ed9642a5b2d7a6ba5f294eb36920af2394
debu <sahadebojyoti12@gmail.com>
2022-12-01 21:24:46 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'react-form';
+  title = 'login registration -form';
+
+   ngform = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email:new FormControl('', [Validators.required, Validators.minLength(3)]),
+    phoneno:new FormControl('',[Validators.required,Validators.minLength(10)]),
+    address:new FormControl('',[Validators.required,Validators.minLength(3)]),
+  });
+  
+  get f(){
+
+    return this.ngform.controls;
+  }
+  
+  mySubmit(){
+    console.log(this.ngform.value);
+    console.log(this.ngform.value);
+    console.log(this.ngform.value);
+    console.log(this.ngform.value);
+  }
+
+
+
 }
